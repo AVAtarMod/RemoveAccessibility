0c01dee9fe4379fc5cd09cf2f2cd8f6bb67cb113
AVAtarMod <Respawner289@gmail.com>
2024-04-05 07:25:24 +0300

    Fix extension code, optimize initialization process.

--- extension.js
+++ extension.js
@@ -2,6 +2,7 @@
  Copyright 2012 Sebastian Ventura
  Copyright 2012 Meng Zhuo
  Copyright 2015 Mario Sanchez Prada
+ Copyright 2024 Grigory Stupnikov
  This file is part of Remove Accessibility.
 
  Remove Accessibility is free software: you can redistribute it and/or modify
@@ -17,62 +18,55 @@
  You should have received a copy of the GNU General Public License
  along with Remove Accessibility.  If not, see <http://www.gnu.org/licenses/>.
 **/
+import { Extension } from 'resource:///org/gnome/shell/extensions/extension.js';
 
-const Lang = imports.lang;
-const Main = imports.ui.main;
+import * as Main from 'resource:///org/gnome/shell/ui/main.js';
 
-const A11yStatusIconHelper = new Lang.Class({
-    Name: 'A11yStatusIconHelper',
+export default class A11yStatusIconHelper extends Extension {
+   constructor(metadata) {
+      super(metadata);
+   }
 
-    _init: function() {
-        // Get a reference to the right accessibility icon.
-        let a11yElement = null;
-        if (typeof Main.panel._statusArea === 'undefined') {
-            a11yElement = Main.panel.statusArea.a11y;
-        } else {
-            a11yElement = Main.panel._statusArea.a11y;
-        }
+   _onIconVisibilityChanged(actor) {
+      if (actor.is_visible()) {
+         actor.hide();
+      }
+   }
 
-        // The ClutterActor representing the icon in the panel.
-        this._iconActor = a11yElement.actor;
+   hideA11yElement() {
+      // Monitor changes to the icon's visibilty, so that we
+      // ensure the icon always keeps hidden, no-matter-what.
+      this._handlerId = this._iconActor.connect('notify::visible',
+         this._onIconVisibilityChanged.bind(this));
+      this._iconActor.hide();
+   }
 
-        // Id for the handler used to connect to GSetting's signals.
-        this._handlerId = 0;
-    },
+   restoreA11yElement() {
+      if (this._handlerId) {
+         this._iconActor.disconnect(this._handlerId);
+         this._handlerId = 0;
+      }
+      this._iconActor.show();
+   }
 
-    _onIconVisibilityChanged: function(actor) {
-        if (actor.is_visible()) {
-            actor.hide();
-        }
-    },
+   enable() {
+      // Get a reference to the right accessibility icon.
+      let a11yElement = null;
+      if (typeof Main.panel._statusArea === 'undefined') {
+         a11yElement = Main.panel.statusArea.a11y;
+      } else {
+         a11yElement = Main.panel._statusArea.a11y;
+      }
 
-    hideA11yElement: function() {
-        // Monitor changes to the icon's visibilty, so that we
-        // ensure the icon always keeps hidden, no-matter-what.
-        this._handlerId = this._iconActor.connect('notify::visible',
-                                                  Lang.bind(this, this._onIconVisibilityChanged));
-        this._iconActor.hide();
-    },
+      // The ClutterActor representing the icon in the panel.
+      this._iconActor = a11yElement.actor;
 
-    restoreA11yElement: function() {
-        if (this._handlerId) {
-            this._iconActor.disconnect(this._handlerId);
-            this._handlerId = 0;
-        }
-        this._iconActor.show();
-    }
-});
+      // Id for the handler used to connect to GSetting's signals.
+      this._handlerId = 0;
+      this.hideA11yElement();
+   }
 
-let _a11yHelper = null;
-
-function init() {
-}
-
-function enable() {
-    _a11yHelper = new A11yStatusIconHelper();
-    _a11yHelper.hideA11yElement();
-}
-
-function disable() {
-    _a11yHelper.restoreA11yElement();
-}
+   disable() {
+      this.restoreA11yElement();
+   }
+};
